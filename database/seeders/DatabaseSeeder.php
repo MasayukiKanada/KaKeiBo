7ee626bdc0dd2ca28cc0c93f07d6e3d4b842e25f
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-02-17 10:22:35 +0900

    secondaryCategoryModelMigration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PrimaryCategorySeeder::class,
+            SecondaryCategorySeeder::class,
         ]);
 
         // \App\Models\User::factory(10)->create();
