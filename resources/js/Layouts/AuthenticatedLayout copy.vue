993dfe6b76ab45eda2bbd3e6aa5684f59ef15388
MasayukiKanada <kanada.masayuki.hata@gmail.com>
2025-03-24 19:19:23 +0900

    spHeader

--- resources/js/Layouts/AuthenticatedLayout copy.vue
+++ resources/js/Layouts/AuthenticatedLayout copy.vue
@@ -1,16 +1,29 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import ApplicationLogoS from '@/Components/ApplicationLogoS.vue';
 import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
-import { Link } from '@inertiajs/vue3';
+import { Link, Head } from '@inertiajs/vue3';
 
 onMounted(() => {
     const btn = window.document.querySelector('#cat_btn');
     if(route().current('categories.index')) {
         btn.classList.add('active');
+    }
+
+    const create_btn = window.document.querySelector('#create_btn');
+    if(route().current('items.create') || route().current('items.show')
+    || route().current('items.edit') || route().current('categories.edit')
+    || route().current('categories.create') || route().current('categories.index') ) {
+        create_btn.classList.add('hidden');
+    }
+
+    const category_btn = window.document.querySelector('#category_btn');
+    if(route().current('categories.index') === false) {
+        category_btn.classList.add('hidden');
     }
 })
 
@@ -19,48 +32,64 @@ const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
+<Head>
+    <link rel="icon" href="/favicon.ico">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+</Head>
+
     <div>
         <div class="min-h-screen bg-gray-100">
+            <!-- Logo -->
+             <div id="site_header" >
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div id="logo" class="shrink-0 sm:flex items-center hidden">
+                        <Link :href="route('chart.daily')">
+                            <ApplicationLogo
+                                class="logo block fill-current text-gray-800 ml-12"
+                            />
+                        </Link>
+                    </div>
+                </div>
+             </div>
             <nav class="bg-white border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
                         <div class="flex">
                             <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
+                            <div class="shrink-0 flex items-center sm:hidden">
+                                <Link :href="route('chart.daily')">
+                                    <ApplicationLogoS
                                         class="block w-12 fill-current text-gray-800"
                                     />
                                 </Link>
                             </div>
-
                             <!-- Navigation Links -->
                             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
                                 <NavLink :href="route('chart.daily')" :active="route().current('chart.daily')">
                                     日別
                                 </NavLink>
                                 <NavLink :href="route('chart.category')" :active="route().current('chart.category')">
                                     カテゴリ別
                                 </NavLink>
-                                <NavLink :href="route('chart.table')" :active="route().current('chart.table')">
-                                    収支表
+                                <NavLink :href="route('chart.index')" :active="route().current('chart.index')">
+                                    チャート
                                 </NavLink>
                                 <NavLink :href="route('items.index')" :active="route().current('items.index')">
                                     仕訳一覧
+                                </NavLink>
+                                <NavLink :href="route('graph')" :active="route().current('graph')">
+                                    グラフ
                                 </NavLink>
                             </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
-                            <div class="ml-3 mr-12">
-                                <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-400 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-500 rounded font-semibold">仕訳入力</Link>
+                            <div class="ml-3 mr-1 md:mr-5">
+                                <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-400 border-0 md:py-1 px-6 focus:outline-none hover:bg-indigo-500 rounded font-semibold">仕訳入力</Link>
                             </div>
-                            <div class="ml-3 mr-12">
-                                <Link id="cat_btn" as="button" :href="route('categories.index')" class="flex ml-auto text-white bg-gray-400 border-0 py-2 px-6 focus:outline-none hover:bg-gray-500 rounded font-semibold">カテゴリ管理</Link>
+                            <div class="ml-3 mr-1 md:mr-3">
+                                <Link id="cat_btn" as="button" :href="route('categories.index')" class="flex ml-auto text-white bg-gray-400 border-0 md:py-1 px-6 focus:outline-none hover:bg-gray-500 rounded font-semibold">カテゴリ管理</Link>
                             </div>
                             <!-- Settings Dropdown -->
                             <div class="ml-3 relative">
@@ -90,9 +119,9 @@ const showingNavigationDropdown = ref(false);
                                     </template>
 
                                     <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                                        <DropdownLink :href="route('profile.edit')"> プロフィール </DropdownLink>
                                         <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
+                                            ログアウト
                                         </DropdownLink>
                                     </template>
                                 </Dropdown>
@@ -138,17 +167,14 @@ const showingNavigationDropdown = ref(false);
                     class="sm:hidden"
                 >
                     <div class="pt-2 pb-3 space-y-1">
-                        <ResponsiveNavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                            Dashboard
-                        </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('chart.daily')" :active="route().current('chart.daily')">
                             日別
                         </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('chart.category')" :active="route().current('chart.category')">
                             カテゴリ別
                         </ResponsiveNavLink>
-                        <ResponsiveNavLink :href="route('chart.table')" :active="route().current('chart.table')">
-                            収支表
+                        <ResponsiveNavLink :href="route('chart.index')" :active="route().current('chart.index')">
+                            チャート
                         </ResponsiveNavLink>
                         <ResponsiveNavLink :href="route('items.index')" :active="route().current('items.index')">
                             仕訳一覧
@@ -168,9 +194,9 @@ const showingNavigationDropdown = ref(false);
                         </div>
 
                         <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('profile.edit')"> プロフィール </ResponsiveNavLink>
                             <ResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
+                                ログアウト
                             </ResponsiveNavLink>
                         </div>
                     </div>
@@ -188,6 +214,14 @@ const showingNavigationDropdown = ref(false);
             <main>
                 <slot />
             </main>
+
+            <!-- Responsive Buttons -->
+            <div class="sm:hidden flex fixed bottom-0 sm:items-center sm:ml-6 w-full z-50">
+                <div class="w-full">
+                    <Link id="create_btn" as="button" :href="route('items.create')" class="block w-full ml-auto text-white bg-indigo-400 border-0 py-1 px-6 focus:outline-none hover:bg-indigo-500 font-semibold">仕訳入力</Link>
+                    <Link id="category_btn" as="button" :href="route('categories.create')" class="block w-full ml-auto mr-6 text-white bg-orange-300 border-0 py-2 px-6 focus:outline-none hover:bg-orange-400 rounded font-semibold">カテゴリ作成</Link>
+                </div>
+            </div>
         </div>
     </div>
 </template>
