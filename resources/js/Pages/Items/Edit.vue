bc07bdf2533e460940c5636d8164fcf3ee274a76
MasayukiKanada <142076884+MasayukiKanada@users.noreply.github.com>
2025-05-13 09:56:05 +0900

    Update Edit.vue

--- resources/js/Pages/Items/Edit.vue
+++ resources/js/Pages/Items/Edit.vue
@@ -135,7 +135,10 @@ const isNotEmpty = obj => {
                                             <div class="p-2 w-full">
                                                 <div class="relative">
                                                     <label for="price" class="leading-7 text-sm text-gray-500">金額<span class="text-red-500">※</span></label>
-                                                    <input type="number" id="price" name="price" v-model="form.price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-500 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    <div class="flex items-center">
+                                                        <input type="number" id="price" name="price" v-model="form.price" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-500 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                        <p class="text-gray-500 px-3 text-base">円</p>
+                                                    </div>
                                                 </div>
                                             </div>
 
